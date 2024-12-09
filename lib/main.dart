1ed2681c49d87402fdf4c2d4eb68a381f1da046b
111coding <jwlee1024@gmail.com>
2024-12-09 18:49:54 +0900

    feat: binding to HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_local_search/ui/home/home_page.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,76 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_search/ui/home/home_view_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        FocusScope.of(context).unfocus();
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          title: TextField(
-            maxLines: 1,
-            onSubmitted: (value) {
-              // TODO 검색하기
-            },
-            decoration: InputDecoration(
-              hintText: '검색어를 입력해 주세요',
-              border: MaterialStateOutlineInputBorder.resolveWith(
-                (states) {
-                  if (states.contains(WidgetState.focused)) {
+    return Consumer(builder: (context, ref, child) {
+      final homeState = ref.watch(homeViewModel);
+      final viewModel = ref.read(homeViewModel.notifier);
+      return GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: TextField(
+              maxLines: 1,
+              onSubmitted: (value) {
+                viewModel.searchLocation(value);
+              },
+              decoration: InputDecoration(
+                hintText: '검색어를 입력해 주세요',
+                border: MaterialStateOutlineInputBorder.resolveWith(
+                  (states) {
+                    if (states.contains(WidgetState.focused)) {
+                      return OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.black),
+                      );
+                    }
                     return OutlineInputBorder(
                       borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(color: Colors.black),
+                      borderSide: BorderSide(color: Colors.grey),
                     );
-                  }
-                  return OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.grey),
-                  );
-                },
+                  },
+                ),
               ),
             ),
           ),
-        ),
-        body: ListView.separated(
-          padding: EdgeInsets.all(20),
-          itemCount: 10, // TODO data binding
-          separatorBuilder: (context, index) => SizedBox(height: 20),
-          itemBuilder: (context, index) {
-            // TODO data binding
-            return GestureDetector(
-              onTap: () {
-                // TODO DetailPage
-              },
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(
-                    color: Colors.grey[300]!,
-                  ),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '코엑스',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
+          body: ListView.separated(
+            padding: EdgeInsets.all(20),
+            itemCount: homeState.locations.length,
+            separatorBuilder: (context, index) => SizedBox(height: 20),
+            itemBuilder: (context, index) {
+              final location = homeState.locations[index];
+              return GestureDetector(
+                onTap: () {
+                  // TODO DetailPage
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(
+                      color: Colors.grey[300]!,
                     ),
-                    Text('문화,예술>컨벤션센터'),
-                    Text('서울특별시 강남구 영동대로 513'),
-                  ],
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        location.title,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(location.category),
+                      Text(location.roadAddress),
+                    ],
+                  ),
                 ),
-              ),
-            );
-          },
+              );
+            },
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
