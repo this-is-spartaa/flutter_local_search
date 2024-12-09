4db8d0edf41ff852d9c40fe8007c8377dbccdd17
111coding <jwlee1024@gmail.com>
2024-12-09 19:11:27 +0900

    feat: add searchByLatLng in homeViewModel

--- lib/ui/home/home_view_model.dart
+++ lib/ui/home/home_view_model.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_local_search/data/model/location.dart';
 import 'package:flutter_local_search/data/repository/location_repository.dart';
+import 'package:flutter_local_search/data/repository/vworld_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeState {
@@ -14,6 +15,15 @@ class HomeViewModel extends Notifier<HomeState> {
   }
 
   final locationRepository = const LocationRepository();
+
+  final vworldRepository = const VworldRepository();
+
+  void searchByLatLng(double lat, double lng) async {
+    final locationResult = await vworldRepository.findByLatLng(lat, lng);
+    if (locationResult.isNotEmpty) {
+      searchLocation(locationResult.first);
+    }
+  }
 
   void searchLocation(String query) async {
     final result = await locationRepository.searchLocation(query);
